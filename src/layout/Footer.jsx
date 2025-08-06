c4b6cf6b01e8160f20412081fc87da6467616df4
unknown <nisa.codesit@gmail.com>
2025-08-06 21:50:13 +0600

    Fix: install @tailwindcss/vite

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,62 +1,67 @@
 import React from 'react'
 import Container from '../components/Container'
 import Flex from '../components/Flex'
+import Images from '../components/Images'
+import logo from '/src/assets/image/Logo.png'
 
 const Footer = () => {
   return (
     <>
-    <div className="py-4 bg-[#F5F5F3]">
-      
-      <Container>
-        <Flex clas>
-          <div className="text-[#6D6D6D] text-[14px] font-normal ">
-            <h3 className='text-black text-[16px] font-bold pb-4'>Menu</h3>
-            <h4>Home</h4>
-            <h4>Shop</h4>
-            <h4>About</h4>
-            <h4>Contact</h4>
-            <h4>Journal</h4>
-          </div>
-          <div className="text-[#6D6D6D] text-[14px] font-normal ">
-            <h3 className='text-black text-[16px] font-bold pb-4'>Shop</h3>
-            <h4>Category 1</h4>
-            <h4>Category 2</h4>
-            <h4>Category 3</h4>
-            <h4>Category 4</h4>
-            <h4>Category 5</h4>
-          </div>
-          <div className="text-[#6D6D6D] text-[14px] font-normal ">
-            <h3 className='text-black text-[16px] font-bold pb-4'>Help</h3>
-            <h4>Privacy Policy</h4>
-            <h4>Terms & Conditions</h4>
-            <h4>Special E-shop</h4>
-            <h4>Shipping</h4>
-            <h4>Secure Payments</h4>
-          </div>
-          <div className="text-[#6D6D6D] text-[14px] font-normal ">
-            <h3 className='text-black text-[16px] font-bold '>(052) 611-5711</h3>
-            <h3 className='text-black text-[16px] font-bold pb-4'>company@domain.com</h3>
-            <h4>575 Crescent Ave. Quakertown, PA 18951</h4>
-           
-          </div>
+      <div className="py-4 bg-[#F5F5F3]">
 
-          <div className="">
-            <img src="/src/assets/image/Logo.png" alt="" className='w-1/2' />
-          </div>
-          
-        </Flex>
-      </Container>
-    </div>
-    
+        <Container>
+          <Flex clas>
+            <div className="text-[#6D6D6D] text-[14px] font-normal ">
+              <h3 className='text-black text-[16px] font-bold pb-4'>Menu</h3>
+              <h4>Home</h4>
+              <h4>Shop</h4>
+              <h4>About</h4>
+              <h4>Contact</h4>
+              <h4>Journal</h4>
+            </div>
+            <div className="text-[#6D6D6D] text-[14px] font-normal ">
+              <h3 className='text-black text-[16px] font-bold pb-4'>Shop</h3>
+              <h4>Category 1</h4>
+              <h4>Category 2</h4>
+              <h4>Category 3</h4>
+              <h4>Category 4</h4>
+              <h4>Category 5</h4>
+            </div>
+            <div className="text-[#6D6D6D] text-[14px] font-normal ">
+              <h3 className='text-black text-[16px] font-bold pb-4'>Help</h3>
+              <h4>Privacy Policy</h4>
+              <h4>Terms & Conditions</h4>
+              <h4>Special E-shop</h4>
+              <h4>Shipping</h4>
+              <h4>Secure Payments</h4>
+            </div>
+            <div className="text-[#6D6D6D] text-[14px] font-normal ">
+              <h3 className='text-black text-[16px] font-bold '>(052) 611-5711</h3>
+              <h3 className='text-black text-[16px] font-bold pb-4'>company@domain.com</h3>
+              <h4>575 Crescent Ave. Quakertown, PA 18951</h4>
 
+            </div>
 
+            <div className="">
+              {/* <img src="/src/assets/image/Logo.png" alt="" className='w-1/2' /> */}
 
+              <Images imgSrc={logo} />
 
+            </div>
+
+          </Flex>
+        </Container>
+      </div>
 
 
 
-    
-    
+
+
+
+
+
+
+
     </>
   )
 }
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Images from '../components/Images'
+import err from '/src/assets/image/error.gif'
 
 const Error = () => {
   return (
-    <div className=' bg-[#d8765d]'>
-      <img src="/src/assets/image/error.gif" alt="" className='w-1/3 m-auto' />
+    <div className="bg-[#d8765d]">
+
+      <div className='  w-1/3 m-auto'>
+        {/* <img src="/src/assets/image/error.gif" alt="" className='w-1/3 m-auto' /> */}
+        <Images imgSrc={err} />
+
+      </div>
     </div>
   )
 }
